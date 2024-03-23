c586fd4eebf6579c468914936c7a3cefb4b74831
OuzZia <ouz.zia06@gmail.com>
2024-03-23 22:01:16 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride yer alabilecek maximum ürün sayısı aşıldı";
+        public static string ProductNameError = "Aynı isimde birden fazla ürün eklenemez";
+        public static string ProductNameAlreadyExists = "Aynı isimde birden fazla ürün eklenemez";
+        public static string CategoryLimitExceded = "Maksimum kategori sayısı limiti aşıldığı için yeni ürün eklenemez";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kullanıcı kaydı başarılı";
+        public static string UserNotFound = "Kullanıcı kaydı bulunamadı";
+        public static string PasswordError = "Şifre hatalı, giriş başarısız";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Bu emaile ait başka bir kayıt mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
+        public static string Updated = "Güncellendi";
+        public static string InvalidEntity = "Kayıt bulunamadı";
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,55 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(),new CategoryManager(new EfCategoryDal()));
+            //var result = productManager.GetProductDetails();
+            //if (result.Success)
+            //{
+            //    foreach (var product in result.Data)
+            //    {
+            //        Console.WriteLine(product.ProductName + " " + product.CategoryName);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result.Message);
+            //}
+            var result = productManager.GetByUnitPrice(5, 20);
+            if (result.Success)
+            {
+                foreach (var item in result.Data)
+                {
+                    Console.WriteLine(item.ProductName + " " + item.UnitPrice);
+                }
+            }
+            //Console.WriteLine("-----------------");
+
+            //foreach (var p1 in productManager.GetAllByCategoryId(5))
+            //{
+            //    Console.WriteLine(p1.ProductName);
+            //}
+            //Console.WriteLine("-----------");
+
+            //foreach (var p2 in productManager.GetByUnitPrice(10,12))
+            //{
+            //    Console.WriteLine(p2.ProductName + " "+ p2.UnitPrice );
+            //}
+
+            //CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            //foreach (var category in categoryManager.GetAll())
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
+        }
+    }
+}
